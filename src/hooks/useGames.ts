ee86a7417bb89655db5b88feea5dc252bdc1908a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-25 14:32:21 +0700

    Infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -16,16 +16,20 @@ export interface Game {
 const apiClient = new APIClient<Game>('/games')
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () => apiClient.getAll({
+    queryFn: ({ pageParam = 1 }) => apiClient.getAll({
       params: {
         genres: gameQuery.genre?.id,
         parent_platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
+        search: gameQuery.searchText,
+        page: pageParam,
       }
     }),
+    getNextPageParam: (lastPage, allPages) => {
+      return allPages.length ? allPages.length + 1 : undefined
+    }
   })
 
 export default useGames
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
+  next: string | null
   results: T[]
 }
 
@@ -19,12 +20,11 @@ class APIClient<T> {
     this.endpoint = endpoint
   }
 
-  getAll = async (config: AxiosRequestConfig) => {
-    const res = await axiosInstance
-      .get<FetchResponse<T>>(this.endpoint, config);
-
-    return res.data;
-  }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient
