0f553fa09405f45dc8a0c3aafb7f11e129d8d39c
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-25 14:58:25 +0700

    Infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -29,7 +29,8 @@ const useGames = (gameQuery: GameQuery) =>
     }),
     getNextPageParam: (lastPage, allPages) => {
       return allPages.length ? allPages.length + 1 : undefined
-    }
+    },
+    staleTime: 24 * 60 * 60 * 1000 //24h
   })
 
 export default useGames
