1e709c9bfba965bbae12a7d41bfd224f7a96d798
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-26 15:22:26 +0700

    Fetch Game Details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,5 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
@@ -23,7 +24,8 @@ const GameCard = ({ game }: Props) => {
         </HStack>
 
         <Heading fontSize="2xl">
-          {game.name} <Emoji rating={game.rating_top} />
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
+          <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
     </Card>
